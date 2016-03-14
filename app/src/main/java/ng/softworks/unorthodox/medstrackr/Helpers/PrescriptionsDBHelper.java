e0f3805c34f4430afee4292b9df8094a1714434a
unorthodox <unorthodox@cyberdude.com>
2016-03-15 00:41:53 +0100

    Enhancements and Bug fixing in the PrescriptionsDBHelper class

--- app/src/main/java/ng/softworks/unorthodox/medstrackr/Helpers/PrescriptionsDBHelper.java
+++ app/src/main/java/ng/softworks/unorthodox/medstrackr/Helpers/PrescriptionsDBHelper.java
@@ -188,5 +188,6 @@ public class PrescriptionsDBHelper extends SQLiteOpenHelper{
         // Updating profile picture url for user with that userName
         return db.update(TABLE_DrugPrescp, values, KEY_ID + " = ?",
                 new String[] { String.valueOf(prescription.Drug_id) });
+
     }
 }
